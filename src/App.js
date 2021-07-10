baa3b383c4069e546ae1df49c6b269a3354a5960
Temiloluwa Onaleye <onaleyetemmy@outlook.com>
2021-07-10 15:20:22 +0100

    Row

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Row from './Row';
 
 function App() {
   return (
     <div className="App">
       <h1>Hey Onaleye Temiloluwa </h1>
+      <Row title="NETFLIX ORIGINALS" />
+      <Row title="Trending Now" />
+      <Row title="Top Rated" />
+      <Row title="Action Movies" />
     </div>
   );
 }
