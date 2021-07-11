e7f128084f91fca4614f91b98eb45314ffc197b1
Temiloluwa Onaleye <onaleyetemmy@outlook.com>
2021-07-11 12:48:00 +0100

    buidling the rows

--- src/Row.js
+++ src/Row.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import { any } from 'prop-types';
+import React, { useState, useEffect } from 'react'
+import axios from './axios';
+import requests from './requests';
+import "./Row.css";
 
-function Row({title}) {
-    const [movies,  setMovies] = 
+const base_url = "https://image.tmdb.org/t/p/original/";
+function Row ({title, fetchUrl}) {
+    const [movies, setMovies] = useState([ ]);
+
+// A snippet of code which runs on specific conditions/variable
+useEffect(( ) => {
+//if [ ], run once when the function is called and dont run again
+async function fetchdata ( ) {
+const request =await axios.get(fetchUrl);
+setMovies(request.data.results);
+return request;
+}
+fetchdata( );
+}, [ fetchUrl]);
+
+console.table(movies);
     return (
-        <div>
-            <h2>{title}</h2>
-            {/*container */}
+
+        <div className="row">
+        <h2>{title}</h2>
+        <div className="row_posters">
+            
+            {/*several row_posters */}
+            {movies.map(movie => (
+
+                <img 
+                key={movie.id}
+                className="row_poster"
+                src={`${base_url}${movie.poster_path}`} alt={movie.name} />
+            ))}
+            </div>
         </div>
     )
 }
